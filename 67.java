1dd7d727136ad3d2b6cf4d8aba2e72a7a37ff4c0
Lucas Rodrigues Monteiro <47841382+lucasrm1981@users.noreply.github.com>
2023-06-05 15:18:34 -0300

    Update 67.java

--- 67.java
+++ 67.java
@@ -5,17 +5,9 @@
 
 import java.util.Scanner;
 
-/**
- *
- * @author lucas.monteiro
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
           double k;
           byte c;
         
